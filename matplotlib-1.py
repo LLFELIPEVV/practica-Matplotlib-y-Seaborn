7b553350c954be3abb91c3aed96a7cc1bc173da6
FELIPE <felipe3133770868@gmail.com>
2025-02-05 16:27:40 -0500

    Agregar funcionalidad para graficar una línea simple utilizando Pyplot en matplotlib-1.py

--- matplotlib-1.py
+++ matplotlib-1.py
@@ -1,5 +1,16 @@
 import matplotlib
-# Matplotlib es una biblioteca de representacion grafica de bajo nivel de Python.
+import matplotlib.pyplot as plt
+import numpy as np
 
+# Matplotlib es una biblioteca de representacion grafica de bajo nivel de Python.
 # Comprobando la version de Matplotlib
 print(matplotlib.__version__)
+
+# Pyplot es un submodulo de Matplotlib que proporciona una interfaz similar a MATLAB.
+# Pyplot contiene la mayoria de las utilidades de Maplotlib.
+# Dibujando una linea simple
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+
+plt.plot(xpoints, ypoints)
+plt.show()
